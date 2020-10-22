408dc245d81819a0625ee27d0066943c84b3e26e
TrueTony <spanchbob@gmail.com>
2020-10-22 23:03:48 +0300

    g

--- bfs.c
+++ bfs.c
@@ -15,6 +15,22 @@ int	create_hash(char *name)
 	return hash;
 }
 
+void	add_link(t_room master, t_room link)
+{
+	t_link *newlin;
+
+	int i = 0;
+	int count_of_link = ();
+	newlin = (t_link*)malloc(sizeof(t_list) * (count_of_link + (count_of_link / 2)));
+	while (i < count_of_link)
+	{
+		newlin[i] = master->lin[i];
+		free(master->link[i]);
+		i++
+	}
+	master->link = newlin;
+}
+
 t_room	*create_room(char *name)
 {
 	t_room *room;
@@ -23,12 +39,13 @@ t_room	*create_room(char *name)
 	room->CheckAnt = 0;
 	room->name_room = name;
 	room->hash = create_hash(name);
-	room->links = (t_link*)malloc(sizeof(t_link));
+	room->links = (t_link*)malloc(sizeof(t_link) * 10);
+	ft_bzero(room->links);
 	room->next = NULL;
 	return room;
 }
 
-void	add_link(t_room *master, t_room *slave)
+void	add_next(t_room *master, t_room *slave)
 {
 	t_room *tmp;
 
@@ -38,7 +55,9 @@ void	add_link(t_room *master, t_room *slave)
 	tmp->next = slave;
 }
 
-void	print_all_links(t_room *room)
+
+
+void	print_all_next(t_room *room)
 {
 	t_room *tmp;
 
@@ -49,7 +68,8 @@ void	print_all_links(t_room *room)
 		printf("%s ", tmp->name_room);
 		tmp = tmp->next;
 	}
-	printf("\n");
+	printf("\nEnd of master`s rooms\n");
+
 }
 
 int		main(void)
@@ -59,16 +79,16 @@ int		main(void)
 	t_room *bob = create_room("Bob");
 	t_room *alice = create_room("Alice");
 	t_room *clair = create_room("Clair");
-	add_link(you, bob);
-	add_link(you, alice);
-	add_link(you, clair);
+//	add_link(you, bob);
+//	add_link(you, alice);
+//	add_link(you, clair);
 
-	t_room *anuj = create_room("Anuj");
-	t_room *peggy = create_room("Peggy");
-	add_link(bob, anuj);
-	add_link(bob, peggy);
+	 t_room *anuj = create_room("Anuj");
+	 t_room *peggy = create_room("Peggy");
+//	 add_link(bob, anuj);
+//	 add_link(bob, peggy);
 
-	// add_link(alice, peggy);
+//	 add_link(alice, peggy);
 
 	// t_room *thom = create_room("Thom");
 	// t_room *jonny = create_room("Jonny");
