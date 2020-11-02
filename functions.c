935978a61a998356c59f0d22f09399b024dcf570
Vladimir <qe.wtsn@gmail.com>
2020-11-02 15:24:25 +0300

    validation update

--- functions.c
+++ functions.c
@@ -52,12 +52,12 @@ int get_hash(char *name)
 	return hash;
 }
 
-//void add_next(t_room *master, t_room *slave)
-//{
-//	t_room *tmp;
-//
-//	tmp = master;
-//	while (tmp->next != NULL)
-//		tmp = tmp->next;
-//	tmp->next = slave;
-//}
+void add_next(t_room *master, t_room *slave)
+{
+	t_room *tmp;
+
+	tmp = master;
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+	tmp->next = slave;
+}
