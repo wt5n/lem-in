ead96d2d1768cc2781ce108f309c9a8cab0ca331
Vladimir <qe.wtsn@gmail.com>
2020-11-02 15:25:09 +0300

    validation update

--- test.c
+++ test.c
@@ -1,7 +1,10 @@
 #include "lem_in.h"
 
-int main ()
+int main (void)
 {
-
+	t_validation *validation;
+	init_room_keeper(&validation);
+	validation->n = (t_room**)ft_memalloc(sizeof(t_room*) * 3000);
+	parser_input(validation);
 	return 0;
 } 
