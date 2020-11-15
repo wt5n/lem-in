e798909b5e6c05b0643cc119f2757f90e888e3dc
hlikely <hlikely@student.21-school.ru>
2020-11-15 19:50:06 +0300

    clear links.c

--- links.c
+++ links.c
@@ -1,27 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   links.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ksenaida <ksenaida@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/15 19:32:52 by ksenaida          #+#    #+#             */
+/*   Updated: 2020/11/15 19:42:05 by ksenaida         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "lem_in.h"
 
-void increase_links(t_room *room)
+void	increase_links(t_room *room)
 {
-	int	**new_links;
+	int	**nl;
 	int i;
 
 	i = -1;
-	if (!(new_links = (int**)ft_memalloc((sizeof(int*) * 2))))
+	if (!(nl = (int**)ft_memalloc((sizeof(int*) * 2))))
 		return ;
-	if	(!(new_links[0] = (int*)ft_memalloc((sizeof(int) * (room->links_count * 2)))))
+	if (!(nl[0] = (int*)ft_memalloc((sizeof(int) * (room->links_count * 2)))))
 		return ;
-	if	(!(new_links[1] = (int*)ft_memalloc((sizeof(int) * (room->links_count * 2)))))
+	if (!(nl[1] = (int*)ft_memalloc((sizeof(int) * (room->links_count * 2)))))
 		return ;
 	while (++i < room->links_count)
-		new_links[0][i] = room->links_id[0][i];
+		nl[0][i] = room->links_id[0][i];
 	free(room->links_id[0]);
 	free(room->links_id[1]);
 	free(room->links_id);
-	room->links_id = new_links;
+	room->links_id = nl;
 	room->links_count = room->links_count * 2;
 }
 
-void add_links(t_room *room, int link)
+void	add_links(t_room *room, int link)
 {
 	int i;
 
@@ -38,17 +50,33 @@ void add_links(t_room *room, int link)
 	add_links(room, link);
 }
 
-void add_two_links(t_room *first, t_room *second)
+void	add_two_links(t_room *first, t_room *second)
 {
 	add_links(first, second->id);
 	add_links(second, first->id);
 }
 
-int     add_links_to_queue(int id, t_queue *queue, t_room_keeper *keeper)
+int		finish_link(t_room_keeper *keeper, t_room *tmp_room, int i)
+{
+	int index;
+
+	keeper->n[tmp_room->links_id[0][i]]->prev_room = tmp_room->id;
+	keeper->n[tmp_room->links_id[0][i]]->in_q = keeper->v_limit;
+	if (tmp_room->id == 1 && tmp_room->links_id[0][i] == 2)
+	{
+		tmp_room->links_id[0][i] = 0;
+		tmp_room->links_id[1][i] = 0;
+		index = find_room(keeper, 2, 1);
+		keeper->n[2]->links_id[0][index] = 0;
+		keeper->n[2]->links_id[1][index] = 0;
+	}
+	return (1);
+}
+
+int		add_links_to_queue(int id, t_queue *queue, t_room_keeper *keeper)
 {
 	t_room	*tmp_room;
 	int		i;
-	int     index;
 
 	i = -1;
 	tmp_room = keeper->n[id];
@@ -57,24 +85,12 @@ int     add_links_to_queue(int id, t_queue *queue, t_room_keeper *keeper)
 		if ((tmp_room->links_id[0][i] != 0) && (tmp_room->links_id[1][i] == 0))
 		{
 			if (tmp_room->links_id[0][i] == 2)
-			{
-                keeper->n[tmp_room->links_id[0][i]]->prev_room = tmp_room->id;
-                keeper->n[tmp_room->links_id[0][i]]->in_q = keeper->v_limit;
-			    if (tmp_room->id == 1 && tmp_room->links_id[0][i] == 2)
-			    {
-			    	tmp_room->links_id[0][i] = 0;
-				    tmp_room->links_id[1][i] = 0;
-				    index = find_room(keeper,  2, 1);
-				    keeper->n[2]->links_id[0][index] = 0;
-				    keeper->n[2]->links_id[1][index] = 0;
-			    }
-                return (1);
-            }
+				return (finish_link(keeper, tmp_room, i));
 			if (keeper->n[tmp_room->links_id[0][i]]->visd < keeper->v_limit &&
 				keeper->n[tmp_room->links_id[0][i]]->in_q < keeper->v_limit)
 			{
-                keeper->n[tmp_room->links_id[0][i]]->prev_room = tmp_room->id;
-                keeper->n[tmp_room->links_id[0][i]]->in_q = keeper->v_limit;
+				keeper->n[tmp_room->links_id[0][i]]->prev_room = tmp_room->id;
+				keeper->n[tmp_room->links_id[0][i]]->in_q = keeper->v_limit;
 				in_queue(queue, tmp_room->links_id[0][i]);
 			}
 		}
